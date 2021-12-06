0de51bc115ca4235695baddd395f9199bdeff1c0
alejandro443 <alejandrolovato@upeu.edu.pe>
2021-12-06 01:28:22 -0500

    clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+///-------------------------------
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
+//-------se importo-------------------------------
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+//-----------------------
+
+//externa
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    //-------
+    HttpClientModule,//se puso
+    FormsModule
+    //////------
   ],
   providers: [],
   bootstrap: [AppComponent]
